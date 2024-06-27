97d7ac9299ceddc06503f694549f9880cf23e574
eis-Jeremy <eis08.jerry@gmail.com>
2024-06-27 20:30:15 +0200

    Menu functionality completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+import { useState } from 'react'
+import Title from './Title'
+import menu from './data'
+import Menu from './Menu'
+import Categories from './Categories'
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))]
+
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
-};
-export default App;
+  const [menuItems, setMenuItems] = useState(menu)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
+
+  return (
+    <main>
+      <section className="menu">
+        <Title title="Our menu" />
+        <Categories categories={categories} filterItems={filterItems}/>
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
+}
+export default App
